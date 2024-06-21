b793a5509a065fa0f7592b940f5c7eefd9b6d35c
Тетяна Владімірова <vladimirova_tetiana@Noutbuk-Tetana.local>
2024-06-21 03:23:07 +0300

    add homework_3.2

---
+++ lesson3/homework_3.2/js/index.js
@@ -0,0 +1,5 @@
+const string1 = prompt("Введіть перший рядок:");
+const string2 = prompt("Введіть другий рядок:");
+const string3 = prompt("Введіть третій рядок:");
+
+console.log(`Рядок 1: ${string1}, Рядок 3: ${string3}, Рядок 2: ${string2}`);
